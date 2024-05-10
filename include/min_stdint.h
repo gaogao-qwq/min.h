4c7ce6ec9b77b8977520ebd0a3c0d605b9e2556a
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-10 22:46:10 +0800

    Feat: Recreate stdint
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

---
+++ include/min_stdint.h
@@ -0,0 +1,21 @@
+#ifndef MIN_STDINT_H_
+#define MIN_STDINT_H_ 1
+
+#include "min_def.h"
+
+#define I8_MIN  	(-128)
+#define I16_MIN 	(-32767-1)
+#define I32_MIN 	(-2147483647-1)
+#define I64_MIN 	(-9223372036854775807-1)
+
+#define I8_MAX  	(127)
+#define I16_MAX 	(32767)
+#define I32_MAX 	(2147483647)
+#define I64_MAX 	(9223372036854775807L)
+
+#define U8_MAX  	(255)
+#define U16_MAX 	(65535)
+#define U32_MAX 	(4294967295U)
+#define U64_MAX 	(18446744073709551615UL)
+
+#endif

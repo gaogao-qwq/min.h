2f7e169251e2b9ab44230679c2dd062eed10333d
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-13 17:55:48 +0800

    Chore: Update qsort test
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- tests/min_qsort.c
+++ tests/min_qsort.c
@@ -1,12 +1,13 @@
 #define NOSTDLIB_BUILD
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+
 #include "../include/min.h"
 
 #define TESTARR_LEN (u32)1e4
 i32 unsorted[TESTARR_LEN];
-i32 arr[10] = {-100,4,-100,6,9,3,2,1,10,7};
+i32 arr[10] = {-100, 4, -100, 6, 9, 3, 2, 1, 10, 7};
 
 static i32 compar(const void *p1, const void *p2) {
 	int a = *(int *)p1, b = *(int *)p2;
@@ -20,28 +21,39 @@ static i32 compar(const void *p1, const void *p2) {
 
 // TEST: min_qsort
 int main(void) {
-	printf("Testing min_qsort...\n");
-	printf("Testing(1/2)...\n");
+	bool flag1 = true, flag2 = true;
+	min_print("Testing min_qsort...\n");
+	//---------------
+	// Test 1
+	//---------------
+	min_print("Testing(1/2)...\n");
 	min_qsort(arr, 10, sizeof(arr[0]), compar);
 	for (u32 i = 1; i < 10; ++i) {
-		if (arr[i] < arr[i-1]) {
+		if (arr[i] < arr[i - 1]) {
 			printf("1st test failed at idx %d", i);
+			flag1 = false;
 			break;
 		}
 	}
 	for (u32 i = 0; i < 10; ++i) {
 		printf("[%d]: %d, ", i, arr[i]);
 	}
-	printf("\nTesting(2/2)...\n");
+	if (flag1) min_print("\nTest 1 passed.\n");
+
+	//---------------
+	// Test 2
+	//---------------
+	min_print("\nTesting(2/2)...\n");
 	srandom(time(nil));
-	for (u32 i = 0; i < TESTARR_LEN; ++i)
-		unsorted[i] = (i32)random();
+	for (u32 i = 0; i < TESTARR_LEN; ++i) unsorted[i] = (i32)random();
 	min_qsort(unsorted, TESTARR_LEN, sizeof(unsorted[0]), compar);
 	for (u32 i = 1; i < TESTARR_LEN; ++i) {
-		if (unsorted[i] < unsorted[i-1]) {
+		if (unsorted[i] < unsorted[i - 1]) {
 			printf("2nd test failed at idx %d", i);
+			flag2 = false;
 			break;
 		}
 	}
+	if (flag2) min_print("\nTest 2 passed.\n");
 	return 0;
 }

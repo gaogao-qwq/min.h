8f62898443e75ca04e9a29ae4f8c5d0ca072eead
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-26 18:14:40 +0800

    Chore: Use min_printf instead of printf
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- tests/min_stdlib.c
+++ tests/min_stdlib.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
-
 #include "../include/min.h"
 
 int main(void) {
 	timeval tv = {0};
 	i32 res = sys_gettimeofday(&tv, nil);
-	printf("%d\n", tv.tv_sec);
+	min_printf("%d\n", tv.tv_sec);
 	char buf[4];
 	res = sys_getrandom(buf, 4u, GRND_NONBLOCK);
 	i32 *rn = (i32 *)buf;
 	if (res != -1) {
-		printf("%d\n", *rn);
+		min_printf("%d\n", *rn);
 	}
-	const char str[] = "Hello, World";
-	sys_write(STDOUT_FILENO, str, 13);
 	return 0;
 }

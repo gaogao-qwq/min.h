468b78de4f31d1a909f5feabd269a215de363944
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-27 16:34:19 +0800

    Chore: Add long convertion flag test case
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- tests/min_stdio.c
+++ tests/min_stdio.c
@@ -4,8 +4,8 @@ static char buf[U16_MAX];
 int main(void) {
 	i32 *arr = min_malloc(10 *  sizeof(i32));
 	min_printf("%%: %%\n");
-	min_printf("signed decimal: %d\n", I64_MIN);
-	min_printf("unsigned decimal: %u\n", U64_MAX);
+	min_printf("signed decimal: %d\nsigned long decimal: %ld\n", I32_MIN, I64_MIN);
+	min_printf("unsigned decimal: %u\nunsigned long decimal: %lu\n", U32_MAX, U64_MAX);
 	min_printf("string: %s\n", "Hello, world!");
 	min_printf("pointer: %p\n", arr);
 	return EXIT_SUCCESS;

5e44b7303111bd128957403b1d13523b1674c9f1
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-28 16:56:23 +0800

    Chore: Add more info to min_vec test
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- tests/min_vec.c
+++ tests/min_vec.c
@@ -1,12 +1,30 @@
 #include "../include/min.h"
+// clang-format off
 
 i32 main(void) {
-	Vec vec = makeEmptyVec(sizeof (i32));
-	for (size_t i = 0; i <= 10000; ++i) {
+	Vec vec = makeEmptyVec(sizeof(i32));
+	ChunkList *chunks = bump_alloced_chunks();
+	min_printf("chunks length: %u\n", chunks->length);
+	for (size_t i = 0; i < chunks->length; ++i) {
+		min_printf("start: %p\nsize: %u\n",
+		           chunks->chunks[i].start,
+		           chunks->chunks[i].size);
+	}
+	for (size_t i = 0; i < 10000; ++i) {
 		vecAppend(&vec, &i);
+	}
+	chunks = bump_alloced_chunks();
+	min_printf("chunks length: %u\n", chunks->length);
+	for (size_t i = 0; i < chunks->length; ++i) {
+		min_printf("start: %p\nsize: %u\n",
+		           chunks->chunks[i].start,
+		           chunks->chunks[i].size);
 	}
 	for (size_t i = 0; i < 10; ++i) {
 		min_printf("%d ", *(int *)vecGet(vec, i));
 	}
-	min_printf("%d", *(int*)vecGet(vec, 10000));
+	min_printf("\n%d\n", *(int *)vecGet(vec, 9999));
+	i32 t = 114514;
+	vecSet(vec, 9999, &t);
+	min_printf("%d\n", *(int *)vecGet(vec, 9999));
 }

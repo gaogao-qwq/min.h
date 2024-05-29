457cd3e210cbe68e63258fec99a7731bcad72ba7
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-29 17:15:56 +0800

    Chore: Add min_recalloc test
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- tests/min_malloc.c
+++ tests/min_malloc.c
@@ -1,29 +1,38 @@
 #include "../include/min.h"
 
 i32 main(void) {
-	min_printf("Testing min_malloc()...\n");
+	min_print("Testing min_malloc()...\n");
 	int *i32arr = min_malloc(10 * sizeof(i32));
 	i64 *i64arr = min_malloc(10 * sizeof(i64));
 	char *str = min_malloc(16 * sizeof(char));
 
 	ChunkList *alloced = bump_alloced_chunks();
 	u32 chunks_count = alloced->length;
-	min_printf("Allocated chunks:\n");
+	min_print("Allocated chunks:\n");
 	for (u32 i = 0; i < chunks_count; ++i) {
 		min_printf("\tstart: %p, size: %u\n", alloced->chunks[i].start,
 			 alloced->chunks[i].size);
 	}
-	min_printf("\n");
+	min_print("\n");
 
-	min_printf("Testing min_free()...\n");
+	min_print("Testing min_realloc()...\n");
+	min_realloc(i64arr, 1024 * sizeof(i64));
+	alloced = bump_alloced_chunks();
+	for (u32 i = 0; i < chunks_count; ++i) {
+		min_printf("\tstart: %p, size: %u\n", alloced->chunks[i].start,
+			 alloced->chunks[i].size);
+	}
+	min_print("\n");
+
+	min_print("Testing min_free()...\n");
 	min_free(i32arr);
 	alloced = bump_alloced_chunks();
 	chunks_count = alloced->length;
-	min_printf("Allocated chunks after min_free:\n");
+	min_print("Allocated chunks after min_free:\n");
 	for (u32 i = 0; i < chunks_count; ++i) {
 		min_printf("\tstart: %p, size: %u\n", alloced->chunks[i].start,
 			 alloced->chunks[i].size);
 	}
-	min_printf("\n");
+	min_print("\n");
 	return 0;
 }

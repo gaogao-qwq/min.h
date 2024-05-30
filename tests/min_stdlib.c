363e521dbea798e7502beb672ca907c16c5aad18
gaogao-qwq <gaogaoqwq@gmail.com>
2024-05-30 16:30:01 +0800

    Chore: add assertion for min_stdlib test
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- tests/min_stdlib.c
+++ tests/min_stdlib.c
@@ -3,12 +3,11 @@
 int main(void) {
 	timeval tv = {0};
 	i32 res = sys_gettimeofday(&tv, nil);
+	min_assert(res != -1);
 	min_printf("%d\n", tv.tv_sec);
+
 	char buf[4];
 	res = sys_getrandom(buf, 4u, GRND_NONBLOCK);
-	i32 *rn = (i32 *)buf;
-	if (res != -1) {
-		min_printf("%d\n", *rn);
-	}
-	return 0;
+	min_assert(res != -1);
+	min_printf("%d\n", *(i32 *)buf);
 }
